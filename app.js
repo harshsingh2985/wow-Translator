8e17d9978d3d8d9b68137234105f78430116a0f7
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-06-30 14:35:53 +0530

    ex-04 wiring with textareas inputs done

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var input=document.querySelector("#submitbtn")
 
+var txtinputs=document.querySelector("#txtareas")
+
+
 function eventhandler(){
-    console.log("you have been clicked")
+    // console.log("you have been clicked")
+    var txtinput=txtinputs.value;
+    console.log("clicked ",txtinput)
+    
+
+
 }
 
 input.addEventListener("click",eventhandler)
