f8d849a2c3cc606c5aa8fa0079f2f0d279333b6d
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-06-30 15:24:55 +0530

    update:ex-08 wire fetch(geturl)(textinputs) is done

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ var input=document.querySelector("#submitbtn");
 var txtinputs=document.querySelector("#txtareas");
 var outputdiv=document.querySelector("#output");
 
+var serverurl="https://api.funtranslations.com/translate/wow.json"
 
+
+function geturl(text){
+    return serverurl + "?" +"text="+ text;
+
+}
 
 function eventhandler(){
     // console.log("you have been clicked")
     var txtinput=txtinputs.value;
     // console.log("clicked ",txtinput)
-    outputdiv.innerText="translated"+txtinput;
+    // outputdiv.innerText="translated"+txtinput;
+
+    // callback function in the javascipt
+    fetch(geturl(txtinput))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedtxt=(json.contents.translated)
+        outputdiv.innerText=translatedtxt
+    })
     
+    // console.log(json.contents.translated))
+
+
+
 
 
 
